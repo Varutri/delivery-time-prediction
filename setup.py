192509860f85a0f57668d63b27c95a667e6749c8
Varutri <varutriparihar@gmail.com>
2025-06-16 20:29:38 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Project predicts the delivery time of your order.',
+    author='Varutri',
+    license='',
+)
